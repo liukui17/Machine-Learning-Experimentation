3f834dcc12e564ea5252542f74194d90464f920a
Kuikui Liu <liukui17@uw.edu>
2016-01-10 13:25:38 -0800

    initial naive bayes

---
+++ src/bayes/NaiveBayesClassifier.java
@@ -0,0 +1,96 @@
+package bayes;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import data.Instance;
+
+public class NaiveBayesClassifier<A, L> {
+
+	Map<L, List<Map<A, List<Instance<A, L>>>>> trainingExamples;
+	
+	Map<L, Integer> labelCounts;
+	
+	int trainingSize;
+	
+	int dimensionality;
+	
+	public NaiveBayesClassifier(List<Instance<A, L>> trainingExamples) {
+		this.trainingSize = trainingExamples.size();
+		this.dimensionality = trainingExamples.get(0).getDimensionality();
+		this.trainingExamples = train(trainingExamples);
+		
+		labelCounts = new HashMap<L, Integer>();
+		for (Instance<A, L> instance : trainingExamples) {
+			L nextLabel = instance.getLabel();
+			if (labelCounts.containsKey(nextLabel)) {
+				labelCounts.put(nextLabel, labelCounts.get(nextLabel) + 1);
+			} else {
+				labelCounts.put(nextLabel, 1);
+			}
+		}	
+	}
+	
+	private Map<L, List<Map<A, List<Instance<A, L>>>>> train(List<Instance<A, L>> examples) {
+		Map<L, List<Map<A, List<Instance<A, L>>>>> training = new HashMap<L, List<Map<A, List<Instance<A, L>>>>>();
+		for (Instance<A, L> example : examples) {
+			L nextLabel = example.getLabel();
+			List<Map<A, List<Instance<A, L>>>> labelSet = training.get(nextLabel);
+			if (labelSet == null) {
+				List<Map<A, List<Instance<A, L>>>> newLabelSet = new ArrayList<Map<A, List<Instance<A, L>>>>(dimensionality);
+				for (int i = 0; i < dimensionality; i++) {
+					Map<A, List<Instance<A, L>>> attributeSet = new HashMap<A, List<Instance<A, L>>>();
+					A nextAttributeValue = example.getAttributeValue(i);
+					List<Instance<A, L>> instances = new LinkedList<Instance<A, L>>();
+					instances.add(example);
+					attributeSet.put(nextAttributeValue, instances);
+					newLabelSet.add(attributeSet);
+				}
+				training.put(nextLabel, newLabelSet);
+			} else {
+				for (int i = 0; i < dimensionality; i++) {
+					A nextAttributeValue = example.getAttributeValue(i);
+					List<Instance<A, L>> instances = labelSet.get(i).get(nextAttributeValue);
+					if (instances != null) {
+						instances.add(example);
+					} else {
+						List<Instance<A, L>> newInstances = new LinkedList<Instance<A, L>>();
+						newInstances.add(example);
+						labelSet.get(i).put(nextAttributeValue, newInstances);
+					}
+				}
+			}
+		}
+		return training;
+	}
+	
+	public L predict(List<A> newData) {
+		L prediction = null;
+		double probability = -Double.MAX_VALUE;
+		for (L label : trainingExamples.keySet()) {
+			double logProbability = Math.log(((double) labelCounts.get(label)) / trainingSize);
+			List<Map<A, List<Instance<A, L>>>> nextLabelSet = trainingExamples.get(label);
+			for (int i = 0; i < newData.size(); i++) {
+				Map<A, List<Instance<A, L>>> nextAttributeSet = nextLabelSet.get(i);
+				double total = 0.0;
+				for (A value : nextAttributeSet.keySet()) {
+					total += nextAttributeSet.get(value).size();
+				}
+				double numerator = 1;
+				List<Instance<A, L>> similar = nextAttributeSet.get(newData.get(i));
+				if (similar != null) {
+					numerator = similar.size();
+				}
+				logProbability += Math.log(numerator / total);
+			}
+			if (logProbability > probability) {
+				prediction = label;
+				probability = logProbability;
+			}
+		}
+		return prediction;
+	}
+}
---
+++ src/experiments/TestNaiveBayes.java
@@ -0,0 +1,40 @@
+package experiments;
+
+import java.util.List;
+
+import bayes.NaiveBayesClassifier;
+import data.Instance;
+import data.MNISTParser;
+
+public class TestNaiveBayes {
+	public static void main(String[] args) {
+		List<int[][]> trainingImages = MNISTParser.readImages(MNISTParser.TRAIN_IMAGES);
+		int[] trainingLabels = MNISTParser.readLabels(MNISTParser.TRAIN_LABELS);
+		List<int[][]> testImages = MNISTParser.readImages(MNISTParser.TEST_IMAGES);
+		int[] testLabels = MNISTParser.readLabels(MNISTParser.TEST_LABELS);
+		
+		List<Instance<Integer,Integer>> trainInstances = MNISTParser.makeInstances(trainingImages, trainingLabels, 0, trainingImages.size());
+		List<Instance<Integer,Integer>> testInstances = MNISTParser.makeInstances(testImages, testLabels, 0, testImages.size());
+		
+		trainingImages = null;
+		trainingLabels = null;
+		testImages = null;
+		testLabels = null;
+
+		long start = System.currentTimeMillis();
+		NaiveBayesClassifier<Integer, Integer> classifier = new NaiveBayesClassifier<Integer, Integer>(trainInstances);
+		System.out.println("Training complete (" + (System.currentTimeMillis() - start) + " milliseconds)");
+		
+		int correct = 0;
+		int total = testInstances.size();
+		for (int i = 0; i < total; i++) {
+			Instance<Integer,Integer> nextInstance = testInstances.get(i);
+			int predicted = classifier.predict(nextInstance.getAttributeValues());
+			int actual = nextInstance.getLabel();
+			if (predicted == actual) {
+				correct++;
+			}
+		}
+		System.out.println("Correct: " + correct + "\tTotal: " + total);
+	}
+}
