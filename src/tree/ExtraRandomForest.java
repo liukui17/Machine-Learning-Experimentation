7f866886da15cdeb2e10157357f7c7ca4a9fd947
Kuikui Liu <liukui17@uw.edu>
2016-01-14 11:15:03 -0800

    random forest built upon the RandomEntropyDecisionTree from previous commit. trains much faster than EntropyRandomForest and much more space efficient than ExtremelyRandomForest. as such, can train with larger committee sizes in still reasonable times and also attains higher classification accuracy. not sure why yet...

---
+++ src/tree/ExtraRandomForest.java
@@ -0,0 +1,54 @@
+package tree;
+
+import java.util.List;
+
+import data.Instance;
+import data.Utils;
+
+public class ExtraRandomForest<A, L> extends RandomForest<A, L> {
+	
+	public static final int DEFAULT_NOMINATION_RATIO = 10;
+	
+	public ExtraRandomForest(List<Instance<A, L>> trainingExamples, int subsetSize, int committeeSize) {
+		super(trainingExamples, subsetSize, committeeSize);
+		int totalHeights = 0;
+		int totalLeaves = 0;
+		for (int i = 0; i < committeeSize; i++) {
+			/*
+			 * Default to nominating only 1/10 the number of total attributes at each iteration.
+			 */
+			DecisionTree<A, L> nextTree = new RandomEntropyDecisionTree<A, L>(Utils.makeSubset(trainingExamples, subsetSize),
+					trainingExamples.get(0).getDimensionality() / DEFAULT_NOMINATION_RATIO);
+			committee.add(nextTree);
+
+			updateStats(nextTree);
+
+			totalHeights += nextTree.getHeight();
+			totalLeaves += nextTree.getLeafCount();
+		}
+		
+		updateAverages(totalHeights, totalLeaves);
+	}
+	
+	public ExtraRandomForest(List<Instance<A, L>> trainingExamples, int subsetSize, List<Integer> nominationCounts) {
+		super(trainingExamples, subsetSize, nominationCounts.size());
+		int totalHeights = 0;
+		int totalLeaves = 0;
+		for (int i = 0; i < nominationCounts.size(); i++) {
+			DecisionTree<A, L> nextTree = new RandomEntropyDecisionTree<A, L>(Utils.makeSubset(trainingExamples, subsetSize),
+					nominationCounts.get(i));
+			committee.add(nextTree);
+			
+			updateStats(nextTree);
+			
+			totalHeights += nextTree.getHeight();
+			totalLeaves += nextTree.getLeafCount();
+		}
+		
+		updateAverages(totalHeights, totalLeaves);
+	}
+
+	public ExtraRandomForest(List<Instance<A, L>> trainingExamples, int committeeSize) {
+		this(trainingExamples, trainingExamples.size(), committeeSize);
+	}
+}
