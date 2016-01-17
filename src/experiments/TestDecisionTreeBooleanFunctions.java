d64437cca974bc65c72eef19f97d221bd8729b4a
Kuikui Liu <liukui17@uw.edu>
2016-01-16 23:25:48 -0800

    moved boolean instance generating function to utils

--- src/experiments/TestDecisionTreeBooleanFunctions.java
+++ src/experiments/TestDecisionTreeBooleanFunctions.java
@@ -1,22 +1,23 @@
 package experiments;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 import data.Instance;
+import data.Utils;
 import decision_tree.DecisionTree;
 
 public class TestDecisionTreeBooleanFunctions {
 	static final Random RANDOM = new Random();
 	static final int DIMENSIONS = 8;
 	static final int TRAIN_SIZE = 32;
+	static final int BOOLEAN_FUNCTION = 0;
 	
 	public static void main(String[] args) {
-		List<Instance<Boolean,Boolean>> andTrain = ANDInstances();
+		List<Instance<Boolean,Boolean>> andTrain = Utils.BooleanInstances(TRAIN_SIZE, DIMENSIONS, BOOLEAN_FUNCTION);
 		DecisionTree<Boolean,Boolean> tree = new DecisionTree<Boolean,Boolean>(andTrain);
 		
-		List<Instance<Boolean,Boolean>> andTest = ANDInstances();
+		List<Instance<Boolean,Boolean>> andTest = Utils.BooleanInstances(TRAIN_SIZE, DIMENSIONS, BOOLEAN_FUNCTION);
 		int correct = 0;
 		for (int i = 0; i < andTest.size(); i++) {
 			boolean predicted = tree.predict(andTest.get(i).getAttributeValues());
@@ -26,20 +27,5 @@ public class TestDecisionTreeBooleanFunctions {
 			}
 		}
 		System.out.println("Correct: " + correct + "\tTotal: " + andTest.size());
-	}
-	
-	public static List<Instance<Boolean,Boolean>> ANDInstances() {
-		List<Instance<Boolean,Boolean>> instances = new ArrayList<Instance<Boolean,Boolean>>(TRAIN_SIZE);
-		for (int i = 0; i < TRAIN_SIZE; i++) {
-			List<Boolean> nextAttributes = new ArrayList<Boolean>();
-			boolean res = true;
-			for (int j = 0; j < DIMENSIONS; j++) {
-				boolean next = RANDOM.nextBoolean();
-				res &= next;
-				nextAttributes.add(next);
-			}
-			instances.add(new Instance<Boolean,Boolean>(nextAttributes, res));
-		}
-		return instances;
 	}
 }
