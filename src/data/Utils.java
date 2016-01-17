da6f7e24bacdf52fec8368f4b54526a30ff3462b
Kuikui Liu <liukui17@uw.edu>
2016-01-16 23:23:34 -0800

    added functions for generating instances of basic boolean operators

--- src/data/Utils.java
+++ src/data/Utils.java
@@ -3,6 +3,7 @@ package data;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class Utils {
 	public static <T> List<T> makeSubset(List<T> superset, int size) {
@@ -13,5 +14,62 @@ public class Utils {
 			subset.add(superset.get(i));
 		}
 		return subset;
+	}
+	
+	public static List<Instance<Double, Boolean>> DoubleAttributeBooleanInstances(int size, int dimensions, int booleanFunction) {
+		Random random = new Random();
+		List<Instance<Double, Boolean>> instances = new ArrayList<Instance<Double, Boolean>>(size);
+		for (int i = 0; i < size; i++) {
+			List<Double> nextAttributes = new ArrayList<Double>();
+			boolean res = true;
+			for (int j = 0; j < dimensions; j++) {
+				boolean next = random.nextBoolean();
+				
+				switch(booleanFunction) {
+				case 0:
+					res &= next;
+					break;
+				case 1:
+					res |= next;
+					break;
+				default:
+					res ^= next;
+				}
+				
+				if (next) {
+					nextAttributes.add(1.0);
+				} else {
+					nextAttributes.add(0.0);
+				}
+			}
+			instances.add(new Instance<Double, Boolean>(nextAttributes, res));
+		}
+		return instances;
+	}
+	
+	public static List<Instance<Boolean, Boolean>> BooleanInstances(int size, int dimensions, int booleanFunction) {
+		Random random = new Random();
+		List<Instance<Boolean, Boolean>> instances = new ArrayList<Instance<Boolean, Boolean>>(size);
+		for (int i = 0; i < size; i++) {
+			List<Boolean> nextAttributes = new ArrayList<Boolean>();
+			boolean res = true;
+			for (int j = 0; j < dimensions; j++) {
+				boolean next = random.nextBoolean();
+				
+				switch(booleanFunction) {
+				case 0:
+					res &= next;
+					break;
+				case 1:
+					res |= next;
+					break;
+				default:
+					res ^= next;
+				}
+				nextAttributes.add(next);
+			}
+			instances.add(new Instance<Boolean, Boolean>(nextAttributes, res));
+		}
+		return instances;
 	}
 }
